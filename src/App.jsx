91e9b8251a45740b168d9cdc3119be8b3de70266
ASMaa-AHMED146 <158354808+ASMaa-AHMED146@users.noreply.github.com>
2025-06-25 15:44:00 +0300

    feat :first

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-
-
+import Contact from "./pages/contact/Contact"
 function App() {
 
   return (
-<div className='flex justify-center items-center h-[100vh] bg-black text-white text-[50px]'>Welcome Every Body</div>
+    <>
+      <Contact/>
+    </>
   )
 }
 
