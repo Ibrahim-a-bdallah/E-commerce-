64e35cdc6a692207208e2df244913843ac371b6c
NoorHaan25 <67436744+mohameddaoudmo@users.noreply.github.com>
2025-06-25 19:26:32 +0300

    create check_out Page
    create Checkout Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Checkout from "./pages/Checkout"
 
 
 function App() {
 
   return (
-<div className='flex justify-center items-center h-[100vh] bg-black text-white text-[50px]'>Welcome Every Body</div>
+<div>
+    <Checkout/>
+</div>
   )
 }
 
