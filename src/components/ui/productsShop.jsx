9e11d5e2dcaf288fdf068906e80a0a29710027dc
NoorHaan25 <67348999+NoorHaan25@users.noreply.github.com>
2025-06-30 14:41:04 +0300

    add products count in shop page

--- src/components/ui/productsShop.jsx
+++ src/components/ui/productsShop.jsx
@@ -2,14 +2,15 @@ import React, { useEffect } from 'react'
 import ProductMain from './ProductMain'
 import { useDispatch, useSelector } from 'react-redux';
 import actGetProducts from '../../store/products/actGetProduct';
-function ProductsShop() {
+function ProductsShop({setCountProducts}) {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(actGetProducts());
+    
   }, [dispatch]);
   const products = useSelector((state) => state.products);
   const featuredProducts = products.products;
-  
+  setCountProducts(featuredProducts.length);// Update the count of products in the shop
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pt-6 justify-items-center'>
       {featuredProducts.map((product) => (
