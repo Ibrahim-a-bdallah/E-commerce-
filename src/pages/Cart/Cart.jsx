d2d0eba8295d6c102b7246bf27d0dd70b7ef2391
Abuhamida <mr6303302@gmail.com>
2025-07-04 02:42:25 +0300

    feat: fix bugs in header ui and handel the cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import CartItem from "@/components/cart-item/CartItem";
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const items = useSelector((state) => state.cart.products);
@@ -20,19 +21,34 @@ const Cart = () => {
             className="text-center py-40
            text-3xl"
           >
-            {" "}
-            Cart Is Empty{" "}
-          </h1>{" "}
+            Cart Is Empty
+          </h1>
         </div>
       ) : (
         <div className="w-5/6 mx-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
           {items?.map((item) => (
             <CartItem key={item.product.id} data={item} />
           ))}
-          <div className=" justify-end">
-            <div className="py-4 text-2xl text-end">Total Prise</div>
-            <div className="py-4 text-2xl text-end text-mainColor">
-              ${totalPrice.toFixed(2)}
+          <div className="w-full flex justify-between">
+            <div className="py-4 text-xl text-end flex gap-3">
+              <Link
+                to={"/checkout"}
+                className=" px-4 py-2 rounded-md bg-[#35AFA0] text-white"
+              >
+                Checkout
+              </Link>
+              <Link
+                to={"/shop"}
+                className="  px-4 py-2 rounded-md bg-[#35AFA0] text-white"
+              >
+                Continue Shopping
+              </Link>
+            </div>
+            <div className=" flex gap-3 justify-end">
+              <div className="py-4 text-2xl text-end">Total Prise</div>
+              <div className="py-4 text-2xl text-end text-mainColor">
+                ${totalPrice.toFixed(2)}
+              </div>
             </div>
           </div>
         </div>
